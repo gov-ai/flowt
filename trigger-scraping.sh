3d04ed63a1982a4b7bcf7f8673c06847d7956908
Asapanna Rakesh <rakeshark22@gmail.com>
2021-06-14 02:45:36 +0530

    [workflow] trigger-scraping.sh

--- trigger-scraping.sh
+++ trigger-scraping.sh
@@ -1,5 +1,5 @@
-echo '🚀 making minor changes to be able to push code and trigger workflow ...'
+echo '🚀 making minor changes to be able to push code and trigger scraping workflow ...'
 echo 'init scraping $(date)' > log.txt
 git add log.txt
-git commit -m"`date +'%Y-%m-%d %H:%M:%S'`"
+git commit -m"trigger scraper `date +'%Y-%m-%d %H:%M:%S'`"
 git push origin workflow/release-scraped-data
