03363705456a18c59fdf76190c212299ae46a06e
Asapanna Rakesh <rakeshark22@gmail.com>
2021-06-16 15:19:50 +0530

    trigger scraper 2021-06-16 15:19:50

---
+++ trigger-scraping-ga.sh
@@ -0,0 +1,8 @@
+echo "setting user config"
+git config --global user.email "rakeshark22@gmail.com"
+git config --global user.name "Asapanna Rakesh"
+echo '🚀 making minor changes to be able to push code and trigger scraping workflow ...'
+echo "init scraping `date +'%Y-%m-%d %H:%M:%S'`" > log.txt
+git add log.txt
+git commit -m"trigger scraper `date +'%Y-%m-%d %H:%M:%S'`"
+git push origin workflow/release-scraped-data
