c4d0fadf45a97eec1d2adde26e87a3ffd2185927
Asapanna Rakesh <rakeshark22@gmail.com>
2021-06-17 01:37:37 +0530

    cleaned

--- trigger-scraping.sh
+++
@@ -1,5 +0,0 @@
-echo '🚀 making minor changes to be able to push code and trigger scraping workflow ...'
-echo "init scraping `date +'%Y-%m-%d %H:%M:%S'`" > log.txt
-git add log.txt
-git commit -m"trigger scraper `date +'%Y-%m-%d %H:%M:%S'`"
-git push origin workflow/release-scraped-data
